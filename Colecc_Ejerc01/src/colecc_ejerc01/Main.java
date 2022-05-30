15fa6de96a6a8a48517f54974c792d94dcec2f81
Gaspar Laborde <gasparutn@gmail.com>
2022-05-30 19:29:39 -0300

    Primer commit

---
+++ Colecc_Ejerc01/src/colecc_ejerc01/Main.java
@@ -0,0 +1,21 @@
+
+package colecc_ejerc01;
+
+import entidades.RazasP;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+    
+    RazasP Serv = new RazasP();
+       
+     Serv.crearRazas();
+     Serv.mostrarRazas();
+        
+    }
+    
+    
+    
+}
---
+++ Colecc_Ejerc01/src/entidades/RazasP.java
@@ -0,0 +1,45 @@
+/*
+Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo String.
+El programa pedirá una raza de perro en un bucle, el mismo se guardará en la lista y
+después se le preguntará al usuario si quiere guardar otro perro o si quiere salir. Si
+decide salir, se mostrará todos los perros guardados en el ArrayList.
+ */
+package entidades;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RazasP {
+    private final Scanner leer; 
+    private final ArrayList<String> razas;
+
+    public RazasP() {
+        this.razas = new ArrayList();
+        this.leer = new Scanner(System.in).useDelimiter("\n");
+    }
+
+  
+
+    public void crearRazas() {
+        
+        System.out.println("ingrese una raza de perro");
+        String tipo= leer.next();
+        System.out.println("La raza es: ");
+        razas.add(tipo);
+
+        }
+    
+    public void mostrarRazas(){
+        
+        for (String Ra : razas) {
+            System.out.println("desea agregar otra raza?");
+            
+            System.out.println(Ra);
+        } 
+
+           System.out.println(razas.size());
+        
+        
+}
+    
+}
